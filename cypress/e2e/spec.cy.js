511b85b7a6a0a38c6e1cb96a2686c9d4c725f2a1
Marlone Akidiva <marlone.akidiva@clearlabs.com>
2023-03-03 16:37:50 +0300

    test rack create page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,22 +1,60 @@
-describe("it should navigate to the login page", () => {
-  beforeEach(() => {
+describe("it should login to the application and create a rack", () => {
+  it("creates a rack", () => {
     cy.visit("http://wgs.app.dev.clearlabs.com/login");
 
+    // login
     cy.get('input[name="email"]').type("century.marlone@clearlabs.com");
     cy.get('input[name="password"]').type("test1234{enter}");
-  });
 
-  it("opens the filter", () => {
-    cy.get("#cl-button-menu-toggle-filter").click();
-  });
-  it("selects sample stage - analyzing", () => {
-    cy.get("#cl-button-menu-toggle-filter").click();
-    cy.get("#cl-field-checkbox-sample-filter-stage-IN_RUN").click();
-    cy.get("#cl-button-submit-apply-filters").click();
-  });
-  it("selects sample stage - complete", () => {
-    cy.get("#cl-button-menu-toggle-filter").click();
-    cy.get("#cl-field-checkbox-sample-filter-stage-COMPLETE").click();
-    cy.get("#cl-button-submit-apply-filters").click();
+    // create a new rack
+    cy.get('a[href*="racks"]').click();
+    cy.get("#cl-icon-rack-create").click();
+    cy.get("div.sc-1tfw9m-2.kAjBSV > label:nth-child(1)").click();
+    cy.get('input[name="barcodeId"]').type(
+      `${Math.floor(Math.random() * (100000 - 10 + 1) + 10)}{enter}`
+    );
+    cy.get("section:nth-child(3) > div.sc-1tfw9m-1.iwCpKj > label").click();
+    cy.get("#cl-button-continue-toggle-protocol").click();
+
+    // sample 1
+    cy.get('input[name="labSampleId"]').type(
+      `${Math.floor(Math.random() * (100000 - 10 + 1) + 10)}{enter}`
+    );
+    cy.get("#cl-field-select-add-sample-analysis > button")
+      .contains("Select Analysis")
+      .click();
+    cy.get("ul").as("analysisList");
+    cy.get("@analysisList")
+      .find("li")
+      .contains("Acinetobacter (4.05 MB)")
+      .click();
+    cy.get("select").select("10 - 20x");
+    cy.get("#cl-lab-wgs-add-rack-sample").click();
+
+    // sample 2
+    cy.get('input[name="labSampleId"]').type(
+      `${Math.floor(Math.random() * (100000 - 10 + 1) + 10)}{enter}`
+    );
+    cy.get("#cl-field-select-add-sample-analysis > button")
+      .contains("Select Analysis")
+      .click();
+    cy.get("@analysisList")
+      .find("li")
+      .contains("Campylobacter (1.73 MB)")
+      .click();
+    cy.get("select").select("30 - 40x");
+    cy.get("#cl-lab-wgs-add-rack-sample").click();
+
+    // sample 3
+    cy.get('input[name="labSampleId"]').type(
+      `${Math.floor(Math.random() * (100000 - 10 + 1) + 10)}`
+    );
+    cy.get("#cl-field-select-add-sample-analysis > button")
+      .contains("Select Analysis")
+      .click();
+    cy.get("@analysisList").find("li").contains("Bacillus (5.68 MB)").click();
+    cy.get("select").select("30 - 40x");
+    cy.get("#cl-lab-wgs-add-rack-sample").click();
+    cy.get("#cl-button-continue-toggle-protocol").click();
   });
 });
